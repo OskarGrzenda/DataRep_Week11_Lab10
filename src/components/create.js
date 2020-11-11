42fc65ff7932341c855a7d275a0b46782bffa78a
OSKAR GRZENDA <G00373428@gmit.ie>
2020-11-11 11:10:02 +0000

    Lab Completed

--- src/components/create.js
+++ src/components/create.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from 'react-bootstrap';
-
+import axios from 'axios';
 export class Create extends React.Component {
 
     constructor()
@@ -55,7 +55,20 @@ export class Create extends React.Component {
     onSubmit(e)
     {
         e.preventDefault();
-        alert("Movie: " + this.state.Title + " " + this.state.Year + " " + this.state.Poster) 
+        alert("Movie: " + this.state.Title + " " + this.state.Year + " " + this.state.Poster)
+
+        const newMovie = {
+            title: this.state.Title,
+            year: this.state.Year,
+            poster: this.state.Poster
+        }
+        axios.post('http://localhost:4000/api/movies', newMovie)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
     }
     render() {
         return (
